e0c5b62178f7eeb11b3b0d4320d9e44a569a7b68
drz <me@drz.se>
2024-03-25 10:40:22 +0100

    Add main application file

---
+++ openpgp_keyhandler/openpgp_keyhandler/application.py
@@ -0,0 +1,99 @@
+from flask import Blueprint, current_app, request
+from argon2 import PasswordHasher
+import os
+import time
+import subprocess
+import logging
+
+from openpgp_keyhandler.validators import is_domain_allowed, is_password_allowed, is_email_allowed
+
+bp = Blueprint("application", __name__, url_prefix="/")
+
+# Configure logging.
+logging.basicConfig(filename="/var/log/ddmail_openpgp_keyhandler.log", format='%(asctime)s: %(levelname)s: %(message)s', level=logging.ERROR)
+
+@bp.route("/hash_data", methods=["POST"])
+def hash_data():
+    if request.method == 'POST':
+        ph = PasswordHasher()
+
+        data = request.form.get('data')
+
+        # Validate password.
+        if is_password_allowed(data) != True:
+            logging.error("hash_data() validation of data failed")
+            return "error: validation of data failed"
+
+        data_hash = ph.hash(data)
+
+        return data_hash
+
+@bp.route("/upload_public_key", methods=["POST"])
+def create_key():
+    if request.method == 'POST':
+        ph = PasswordHasher()
+
+        # Get post form data.
+        fingerprint = request.form.get('fingerprint')
+        password = request.form.get('password')
+
+        # Validate fingerprint.
+        if is_fingeprint_allowed(fingerprint) != True:
+            logging.error("upload_public_key() fingerprint validation failed")
+            return "error: fingerprint validation failed"
+
+        # Validate password.
+        if is_password_allowed(password) != True:
+            logging.error("upload_public_key() password validation failed")
+            return "error: password validation failed"
+
+        # Check if password is correct.
+        try:
+            if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
+                time.sleep(1)
+                logging.error("upload_public_key() wrong password")
+                return "error: wrong password"
+        except:
+            time.sleep(1)
+            logging.error("upload_public_key() wrong password")
+            return "error: wrong password"
+        time.sleep(1)
+
+        # Upload public key.
+        logging.debug("upload_public_key() done")
+        return "done"
+
+@bp.route("/remove_public_key", methods=["POST"])
+def change_password_on_key():
+    if request.method == 'POST':
+        ph = PasswordHasher()
+
+        # Get post form data.
+        fingerprint = request.form.get('fingerprint')
+        password = request.form.get('password')
+
+        # Validate fingerprint.
+        if is_fingerprint_allowed(email) != True:
+            logging.error("remove_public_key() fingerprint validation failed")
+            return "error: fingerprint validation failed"
+
+        # Validate password.
+        if is_password_allowed(password) != True:
+            logging.error("remove_public_key() password validation failed")
+            return "error: password validation failed"
+
+        # Check if password is correct.
+        try:
+            if ph.verify(current_app.config["PASSWORD_HASH"], password) != True:
+                time.sleep(1)
+                logging.error("change_password_on_key() wrong password")
+                return "error: wrong password"
+        except:
+            time.sleep(1)
+            logging.error("change_password_on_key() wrong password")
+            return "error: wrong password"
+        time.sleep(1)
+
+        # Remove public key.
+        logging.debug("remove_public_key() done")
+        return "done"
