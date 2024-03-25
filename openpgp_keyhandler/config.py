9a7af6f1c79de762e52b1f344bbc39b1c753a519
drz <me@drz.se>
2024-03-25 10:38:38 +0100

    Add config.py

---
+++ openpgp_keyhandler/config.py
@@ -0,0 +1,11 @@
+class Prod():
+    SECRET_KEY = 'change_me'
+    PASSWORD_HASH = 'change_me'
+
+class Test():
+    SECRET_KEY = 'change_me'
+    PASSWORD_HASH = 'change_me'
+
+class Dev():
+    SECRET_KEY = 'change_me'
+    PASSWORD_HASH = 'change_me'
