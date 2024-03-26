c1a4a7e4f12561ae276bd37f2d4c55f4b0b47e89
drz <me@drz.se>
2024-03-26 09:22:16 +0100

    Add validator for openpgp key fingerprint

--- openpgp_keyhandler/openpgp_keyhandler/validators.py
+++ openpgp_keyhandler/openpgp_keyhandler/validators.py
@@ -10,59 +10,16 @@ def is_password_allowed(password):
 
     return True
 
-# Validate domain names. Only allow the following chars: a-z, 0-9 and .-
-def is_domain_allowed(domain):
-    if not len(domain) > 3:
+# Validate openpgp public key fingerprint string. Only allow the following chars: A-Z, 0-9
+def is_fingerprint_allowed(fingerprint):
+    # Fingerprint string should be 40 char.
+    allowed_len = 40
+    if len(fingerprint) != allowed_len:
         return False
 
-    if domain.startswith('.') or domain.startswith('-'):
-        return False
-    if domain.endswith('.') or domain.endswith('-'):
-        return False
-    if '--' in domain:
-        return False
-    if '..' in domain:
-        return False
-
-    if domain.find(".") == -1:
-        return False
-
-    pattern = re.compile(r"[a-z0-9.-]")
-    for char in domain:
-        if not re.match(pattern, char):
-            return False
-
-    return True
-
-# Validate email address. Only allow the following chars: a-z, 0-9 and @.-
-def is_email_allowed(email):
-    if not len(email) > 6:
-        return False
-
-    if email.count('@') != 1:
-        return False
-    if email.startswith('.') or email.startswith('@') or email.startswith('-'):
-        return False
-    if email.endswith('.') or email.endswith('@') or email.endswith('-'):
-        return False
-
-    # Validate email part of email.
-    splitted_email = email.split('@')
-    if splitted_email[0].startswith('.') or splitted_email[0].startswith('-'):
-        return False
-    if splitted_email[0].endswith('.') or splitted_email[0].endswith('-'):
-        return False
-    if '--' in splitted_email[0]:
-        return False
-    if '..' in splitted_email[0]:
-        return False
-
-    # Validate Domain part of email.
-    if is_domain_allowed(splitted_email[1]) != True:
-        return False
-
-    pattern = re.compile(r"[a-z0-9@.-]")
-    for char in email:
+    # Only allow A-Z, 0-9
+    pattern = re.compile(r"[A-Z0-9]")
+    for char in fingerprint:
         if not re.match(pattern, char):
             return False
 
