ab837c1d4d0bb79acf5f57cc5588fcd7b5f66dcb
drz <me@drz.se>
2024-03-29 13:38:43 +0100

    Add gnupg home

--- openpgp_keyhandler/config.py
+++ openpgp_keyhandler/config.py
@@ -1,11 +1,14 @@
 class Prod():
     SECRET_KEY = 'change_me'
     PASSWORD_HASH = 'change_me'
+    GNUPG_HOME = 'change_me'
 
 class Test():
     SECRET_KEY = 'change_me'
     PASSWORD_HASH = 'change_me'
+    GNUPG_HOME = 'change_me'
 
 class Dev():
     SECRET_KEY = 'change_me'
     PASSWORD_HASH = 'change_me'
+    GNUPG_HOME = 'change_me'
