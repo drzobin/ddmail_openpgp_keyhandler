4693283131dcb4e9519c7aac540f2962b973b756
drzobin <me@drz.se>
2025-03-08 23:57:13 +0100

    Move to pyproject.toml

--- src/ddmail_openpgp_keyhandler/__init__.py
+++ src/ddmail_openpgp_keyhandler/__init__.py
@@ -1,22 +1,35 @@
 import os
+import toml
+import sys
 from flask import Flask
 
 
-def create_app(test_config=None):
-    """Create and configure an instance of the Flask application dmcp_keyhandler."""
+def create_app(config_file = None, test_config = None):
+    """Create and configure an instance of the Flask application ddmail_openpgp_keyhandler."""
     app = Flask(__name__, instance_relative_config=True)
 
-    # Set app configurations from configuration file config.py
+    # Parse toml config file.
+    toml_config = None
+    with open(config_file, 'r') as f:
+        toml_config = toml.load(f)
+
+    # Set app configurations from toml config file.
     mode=os.environ.get('MODE')
     if mode == "PRODUCTION":
-        app.config.from_object("config.Prod")
+        app.config["SECRET_KEY"] = toml_config["PRODUCTION"]["SECRET_KEY"]
+        app.config["PASSWORD_HASH"] = toml_config["PRODUCTION"]["PASSWORD_HASH"]
+        app.config["GNUPG_HOME"] = toml_config["PRODUCTION"]["GNUPG_HOME"]
     elif mode == "TESTING":
-        app.config.from_object("config.Test")
+        app.config["SECRET_KEY"] = toml_config["TESTING"]["SECRET_KEY"]
+        app.config["PASSWORD_HASH"] = toml_config["TESTING"]["PASSWORD_HASH"]
+        app.config["GNUPG_HOME"] = toml_config["TESTING"]["GNUPG_HOME"]
     elif mode == "DEVELOPMENT":
-        app.config.from_object("config.Dev")
+        app.config["SECRET_KEY"] = toml_config["DEVELOPMENT"]["SECRET_KEY"]
+        app.config["PASSWORD_HASH"] = toml_config["DEVELOPMENT"]["PASSWORD_HASH"]
+        app.config["GNUPG_HOME"] = toml_config["DEVELOPMENT"]["GNUPG_HOME"]
     else:
         print("Error: you need to set env variabel MODE to PRODUCTION/TESTING/DEVELOPMENT")
-        exit(1)
+        sys.exit(1)
     
     app.secret_key = app.config["SECRET_KEY"]
 
@@ -27,7 +40,7 @@ def create_app(test_config=None):
         pass
 
     # Apply the blueprints to the app
-    from openpgp_keyhandler import application
+    from ddmail_openpgp_keyhandler import application
     app.register_blueprint(application.bp)
 
     return app 
