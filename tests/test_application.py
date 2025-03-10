d60f12699ddaa6f81c42463bc384c1aa3423e7ff
drzobin <me@drz.se>
2025-03-10 18:21:38 +0100

    Add tests

---
+++ tests/test_application.py
@@ -0,0 +1,17 @@
+from flask import current_app
+import pytest
+import os
+
+def test_upload_public_key(client):
+    response = client.post("/upload_public_key", data={"public_key":"nopublickey","keyring":"nokeyring","password":"wrong password"})
+    assert response.status_code == 200
+    assert b"error: password validation failed" in response.data
+    
+    response = client.post("/upload_public_key", data={"public_key":"no public key","keyring":"nokeyring","password":"wrongpassword"})
+    assert response.status_code == 200
+    assert b"error: public key validation failed" in response.data
+   
+    fake_pubkey = "-----BEGIN PGP PUBLIC KEY BLOCK-----aB1+/=-----END PGP PUBLIC KEY BLOCK-----"
+    response = client.post("/upload_public_key", data={"public_key":fake_pubkey,"keyring":"no keyring","password":"wrongpassword"})
+    assert response.status_code == 200
+    assert b"error: keyring validation failed" in response.data
